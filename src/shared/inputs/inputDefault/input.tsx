e34da575b2fb21927e94e55fcd97d6af124bed81
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2024-08-25 00:00:25 +0100

    Adicionando o component input

---
+++ src/shared/inputs/inputDefault/input.tsx
@@ -0,0 +1,11 @@
+import { Input } from "antd";
+
+const InputDefault = () => {
+    return (
+        <div>
+            <Input size="large" placeholder="large size" />
+        </div>
+    );
+};
+
+export default InputDefault;
