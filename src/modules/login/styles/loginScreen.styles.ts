467cddd07a832ca3b3430d847f4662d55a6a0c37
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2024-08-24 17:28:32 +0100

    intalação da extenção do vscode style components

---
+++ src/modules/login/styles/loginScreen.styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const BackgroundImage = styled.img`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+`;
