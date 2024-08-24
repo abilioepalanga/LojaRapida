2e9c50b98122c9cd5d8ec3d2e049f20a4a1f0668
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2024-08-24 17:12:21 +0100

    Funcionando o componente Login

--- src/modules/login/screems/LoginScreen.tsx
+++ src/modules/login/screems/LoginScreen.tsx
@@ -1,18 +1,9 @@
 const LoginScreen = () => {
-    const { login, loading, error } = useAuth();
-
-    const handleLogin = () => {
-        login();
-    };
-
     return (
         <div>
             <h1>Login</h1>
-            <button onClick={handleLogin} disabled={loading}>
-                Login
-            </button>
-            {error && <p>{error}</p>}
         </div>
     );
 };
+
 export default LoginScreen;
