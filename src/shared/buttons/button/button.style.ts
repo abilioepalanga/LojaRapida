77e3cc49763a342fec290b388a18c4cdfd4f9c2d
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2024-08-25 23:07:45 +0100

    Dados do component butão

--- src/shared/buttons/button/button.style.ts
+++ src/shared/buttons/button/button.style.ts
@@ -1,5 +1,18 @@
-import { Button, Flex } from 'antd';
+import { Button, Flex } from "antd";
+import styled from "styled-components";
 
-
-
-export const ButtonAtd = styled(button)`
+export const ButtonAtd = styled(Button)`
+    width: 320px;
+    height: 40px;
+    background-color: #4caf50;
+    border-radius: 5px;
+    color: white;
+    font-size: 16px;
+    font-weight: 500;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    border: none;
+    &:hover {
+        background-color: #45a049;
+    }
+`;
