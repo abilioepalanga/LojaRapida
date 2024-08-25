afb3e8815068438b6b6161349c54cccd0c8dbd69
abilioepalanga <148195517+abilioepalanga@users.noreply.github.com>
2024-08-25 01:20:57 +0100

    Erros por corrigir no input sytles

---
+++ src/shared/inputs/inputDefault/input.styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const BoxInput = styled.div`;
+
+export const TitleInput = styled.h3`
+ fontFamily: 'Proppins';
+ font-style: normal;
+ font-weight: 500;
+ line-height: 21px;
+ font-size: 14px;
+ margin - bottom: 5px;
+    
+`;
--- src/shared/inputs/inputDefault/input.tsx
+++ src/shared/inputs/inputDefault/input.tsx
@@ -1,9 +1,14 @@
 import { Input } from "antd";
+import { BoxInput, TitleInput } from "./styles";
 
 const InputDefault = () => {
     return (
         <div>
-            <Input size="large" placeholder="large size" />
+            <Input size="large" placeholder="Digite o seu nome" />
+            <BoxInput>
+                <TitleInput>Nome</TitleInput>
+                <Input size="large" placeholder="Digite o seu nome" />
+            </BoxInput>
         </div>
     );
 };
